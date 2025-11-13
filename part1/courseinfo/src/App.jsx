6ef5adf75b27964b3591a7c0dfe7ebcacf95779e
sandrasong <chromsketch@gmail.com>
2025-11-13 12:27:08 -0500

    1.4: modify the variable to be an array

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,36 +5,38 @@ const Part = (props) => (<p>{props.name} {props.exercises}</p>)
 const  Content = (props) => {
   return (
     <>
-      <Part {...props.part1} />
-      <Part {...props.part2} />
-      <Part {...props.part3} />
+      <Part {...props.parts[0]} />
+      <Part {...props.parts[1]} />
+      <Part {...props.parts[2]} />
     </>
   )
 }
 
-const Total = (props) => (<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>)
+const Total = (props) => (<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>)
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
 
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Total parts={parts} />
     </div>
   )
 }
