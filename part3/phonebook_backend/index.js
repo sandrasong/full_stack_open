18d1489e3a9875e6796152043ab5e903b656dfe9
sandrasong <chromsketch@gmail.com>
2025-11-27 15:54:15 -0500

    3.8 phonebook: customise morgan logging format

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -26,7 +26,9 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan("tiny"))
+
+morgan.token("content", (req) => (JSON.stringify(req.body)))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 
 app.get('/info', (request, response) => {
   response.type('text/plain')
