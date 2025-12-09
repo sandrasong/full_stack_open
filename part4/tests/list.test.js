2f6505078c9d5135c94f38b7248ba5ddf4d500aa
sandrasong <chromsketch@gmail.com>
2025-12-08 23:17:03 -0500

    4.4 totalLikes function unit test: write unit test of a totalLikes function that calculate the sum of likes of a list of blogs

--- part4/tests/list.test.js
+++ part4/tests/list.test.js
@@ -6,3 +6,49 @@ test("dummy returns one", () => {
   const blogs = []
   assert.strictEqual(listHelper.dummy(blogs), 1)
 })
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const biggerList = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Use console.log more',
+      author: 'full stack open',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 6,
+      __v: 0
+    }
+  ]
+
+  test("of empty list is zero", () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(biggerList)
+    assert.strictEqual(result, 11)
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = (blogs) => {
   return 1
 }
 
-export default { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, currentBlog) => total + currentBlog.likes, 0)
+}
+
+export default { dummy, totalLikes }
