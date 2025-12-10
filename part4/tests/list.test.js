ff3118dc3c5ff9c08142e32f789aefbeff5590f2
sandrasong <chromsketch@gmail.com>
2025-12-10 10:17:51 -0500

    4.7 mostLikes: unit test of mostLikes function which returns the author whose blogs have most likes

--- part4/tests/list.test.js
+++ part4/tests/list.test.js
@@ -1,6 +1,7 @@
 import { test, describe } from "node:test"
 import assert from "node:assert"
 import listHelper from "../utils/list_helper.js"
+import { log } from "node:console"
 
 test("dummy returns one", () => {
   const blogs = []
@@ -34,6 +35,14 @@ const biggerList = [
     url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
     likes: 6,
     __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f6',
+    title: 'Open console on browser',
+    author: 'full stack open',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 7,
+    __v: 0
   }
 ]
 
@@ -49,7 +58,7 @@ describe("total likes", () => {
 
   test("of a bigger list is calculated right", () => {
     const result = listHelper.totalLikes(biggerList)
-    assert.strictEqual(result, 11)
+    assert.strictEqual(result, 18)
   })
 })
 
@@ -65,7 +74,7 @@ describe("favorite blog", () => {
 
   test("of a bigger list, return most the blog with most likes", () => {
     const result = listHelper.favoriteBlog(biggerList)
-    assert.deepStrictEqual(result, biggerList[1])
+    assert.deepStrictEqual(result, biggerList[2])
   })
 })
 
@@ -81,5 +90,22 @@ describe("most blogs", () => {
 
   test("return the author with largest amount of blogs", () => {
     const result = listHelper.mostBlogs(biggerList)
+    assert.deepStrictEqual(result, {author: "full stack open", blogs: 2})
+  })
+})
+
+describe("most blogs", () => {
+  test("of empty list is {}", () => {
+    assert.deepStrictEqual(listHelper.mostLikes([]), {})
+  })
+
+  test("when list has only one blog, return this blog's author", () => {
+    const result = listHelper.mostLikes(listWithOneBlog)
+    assert.deepStrictEqual(result, {author: listWithOneBlog[0].author, likes: 5})
+  })
+
+  test("return the author with largest amount of blogs", () => {
+    const result = listHelper.mostLikes(biggerList)
+    assert.deepStrictEqual(result, { author: 'full stack open', likes: 13 })
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -23,4 +23,15 @@ const mostBlogs = (blogs) => {
   mostBlogs.blogs > author.blogs ? mostBlogs : author, {})
 }
 
-export default { dummy, totalLikes, favoriteBlog, mostBlogs }
+
+const mostLikes = (blogs) => {
+  const groupedByAuthor = lodash.groupBy(blogs, "author")
+  const authorLikes = lodash.mapValues(groupedByAuthor, blogArray => lodash.sumBy(blogArray, "likes"))
+  const authorList = lodash.map(authorLikes, (value, key) => ({
+    author: key,
+    likes: value
+  }))
+  return authorList.reduce((mostLikes, author) => 
+  mostLikes.likes > author.likes ? mostLikes : author, {})
+}
+export default { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
